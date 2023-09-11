ca72ffed846518fa4ecbd74c50661af438cfcf60
khosta77 <puwerfulpants@gmail.com>
2023-09-11 09:17:42 +0300

    Разкомментировал задание связанное с первым номером

--- matlab/chapter_3.m
+++ matlab/chapter_3.m
@@ -4,50 +4,50 @@ f = sin(x)./x
 g = exp(-x).*cos(x)
 
 % В отдельных окнах
-% figure;
-% f1 = f(find(f > 0)); xf1 = x(find(f > 0));
-% f2 = f(find(f < 0)); xf2 = x(find(f < 0));
-% plot(xf1, f1, 'bo-', xf2, f2, 'rx-');
-% title('График 1');
-% legend('f(x) > 0', 'f(x) < 0', 'Location',"best");
-% xlabel('мм');
-% ylabel('м');
-% grid on;
-% figure;
-% g1 = g(find(g > 0)); xg1 = x(find(g > 0));
-% g2 = g(find(g < 0)); xg2 = x(find(g < 0));
-% plot(xg1, g1, 'ms-.', xg2, g2, 'k*-');
-% title('График 2');
-% legend('g(x) > 0', 'g(x) < 0', 'Location',"best");
-% xlabel('м^3');
-% ylabel('м_1');
-% grid on;
-% 
-% % 
-% figure;
-% plot(xf1, f1, 'bo-', xf2, f2, 'rx-', xg1, g1, 'ms-.', xg2, g2, 'k*-');
-% title('График 3');
-% legend('f(x) > 0', 'f(x) < 0', 'g(x) > 0', 'g(x) < 0', 'Location',"best");
-% xlabel('м^3');
-% ylabel('м_1');
-% grid on;
-% 
-% %
-% figure;
-% subplot(2, 1, 1);
-% plot(xf1, f1, 'bo-', xf2, f2, 'rx-');
-% title('График 1');
-% legend('f(x) > 0', 'f(x) < 0', 'Location',"best");
-% xlabel('мм');
-% ylabel('м');
-% grid on;
-% subplot(2, 1, 2);
-% plot(xg1, g1, 'ms-.', xg2, g2, 'k*-');
-% title('График 2');
-% legend('g(x) > 0', 'g(x) < 0', 'Location',"best");
-% xlabel('м^3');
-% ylabel('м_1');
-% grid on;
+figure;
+f1 = f(find(f > 0)); xf1 = x(find(f > 0));
+f2 = f(find(f < 0)); xf2 = x(find(f < 0));
+plot(xf1, f1, 'bo-', xf2, f2, 'rx-');
+title('График 1');
+legend('f(x) > 0', 'f(x) < 0', 'Location',"best");
+xlabel('мм');
+ylabel('м');
+grid on;
+figure;
+g1 = g(find(g > 0)); xg1 = x(find(g > 0));
+g2 = g(find(g < 0)); xg2 = x(find(g < 0));
+plot(xg1, g1, 'ms-.', xg2, g2, 'k*-');
+title('График 2');
+legend('g(x) > 0', 'g(x) < 0', 'Location',"best");
+xlabel('м^3');
+ylabel('м_1');
+grid on;
+
+% В одном, на одном
+figure;
+plot(xf1, f1, 'bo-', xf2, f2, 'rx-', xg1, g1, 'ms-.', xg2, g2, 'k*-');
+title('График 3');
+legend('f(x) > 0', 'f(x) < 0', 'g(x) > 0', 'g(x) < 0', 'Location',"best");
+xlabel('м^3');
+ylabel('м_1');
+grid on;
+
+% В одном на разных
+figure;
+subplot(2, 1, 1);
+plot(xf1, f1, 'bo-', xf2, f2, 'rx-');
+title('График 1');
+legend('f(x) > 0', 'f(x) < 0', 'Location',"best");
+xlabel('мм');
+ylabel('м');
+grid on;
+subplot(2, 1, 2);
+plot(xg1, g1, 'ms-.', xg2, g2, 'k*-');
+title('График 2');
+legend('g(x) > 0', 'g(x) < 0', 'Location',"best");
+xlabel('м^3');
+ylabel('м_1');
+grid on;
 
 %% 2
 [X, Y] = meshgrid(-1:0.05:1, -1:0.05:1);
